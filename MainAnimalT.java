25508bedbfd0ec5c798de38d253b88ec73457606
shyianenko <93085101+shyianenko@users.noreply.github.com>
2023-11-20 16:57:59 +0200

    Update MainAnimalT.java

--- MainAnimalT.java
+++ MainAnimalT.java
@@ -5,7 +5,10 @@ public class MainAnimalT {
         AnimalT catt = new Catt("Tom","black", 5,7 );
         AnimalT dogg = new Dogg("Spyke","grey", 15,5);
 
-        System.out.println(catt + " voice is: " + catt.getVoice() + " can jump to " + catt.jump() + " meters " + catt.repeat());
-        System.out.println(dogg + " voice is: " + dogg.getVoice() + " can jump to " + dogg.jump() + " meters " + dogg.repeat());
+        System.out.println(catt);
+        catt.repeat(3);
+        
+        System.out.println(dogg);
+        dogg.repeat(2);
     }
 }
